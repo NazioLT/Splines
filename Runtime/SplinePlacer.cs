ec0a0ffa3a6a8d981db439d6b41fe1d00f9532ad
Henno Théo <zoupeurletroll@gmail.com>
2023-09-22 21:54:22 +0200

    uniform and distance placement

--- Runtime/SplinePlacer.cs
+++ Runtime/SplinePlacer.cs
@@ -10,6 +10,8 @@ namespace Nazio_LT.Splines
     {
         [SerializeField] private SplineBehaviour m_spline = null;
 
+        [Space] [SerializeField] private bool m_placeUniform = true;
+
         private void Update()
         {
             if (m_spline)
@@ -19,15 +21,19 @@ namespace Nazio_LT.Splines
         private void HandleUpdatePlacer()
         {
             int objectCount = transform.childCount;
-            
-            if(objectCount == 0)
+
+            if (objectCount == 0)
                 return;
-            
+
             float distancePerObject = 1f / (float)objectCount;
             for (int i = 0; i < objectCount; i++)
             {
                 Transform childToPlace = transform.GetChild(i);
-                childToPlace.position = m_spline.EvaluateUniform(distancePerObject * i);
+
+                if (m_placeUniform)
+                    childToPlace.position = m_spline.EvaluateUniform(distancePerObject * i);
+                else 
+                    childToPlace.position = m_spline.EvaluateDistance(1f * i);
             }
         }
     }
